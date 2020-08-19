5e49cca1d707369b7c2dfeae18b40aa3529669ec
Chirag Kushwaha <chiragkushwaha1811@gmail.com>
2020-08-19 15:08:01 +0530

    commit

---
+++ chapter1.py
@@ -0,0 +1,33 @@
+import cv2
+
+print("package Imported")
+
+# using a webcam
+# cap = cv2.VideoCapture(0)
+# cap.set(3, 640)
+# cap.set(4, 480)
+# cap.set(10, 100)
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+# load a video
+# cap = cv2.VideoCapture("Resources/test_video.mp4")
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+
+# load a image
+# img = cv2.imread("Resources/lena.png")
+#
+# cv2.imshow("Output", img)
+# cv2.waitKey(0)
+
+
+
